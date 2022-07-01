7631a0382600039ac8942cecf09e979682fce4bc
spykelion <ndilionel6@gmail.com>
2022-07-01 11:41:46 +0100

    update route. Remove strict mode

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.Router({ strict: true });
+const router = express.Router();
 const todo = require('../controllers/todo.controller')
 
 router
